d147149c00b14c83fe7108cb4c82ceedab8d1d87
ROHIT RAI <rohitrai2948@gmail.com>
2026-04-01 22:45:47 +0530

    chore(config): add Jest, Tailwind,  and update project configs and ESLint

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,14 @@
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+import { getSentryExpoConfig } from '@sentry/react-native/metro';
+import { withUniwindConfig } from 'uniwind/metro';
 
+// Use Sentry's config as the base (it extends the default config internally)
+// i.e. getDefaultConfig from 'expo/metro-config';
 const config = getSentryExpoConfig(__dirname);
 
-module.exports = config;
+export default withUniwindConfig(config, {
+  // relative path to your global.css file (from previous step)
+  cssEntryFile: './src/global.css',
+  // (optional) path where we gonna auto-generate typings
+  // defaults to project's root
+  dtsFile: './src/uniwind-types.d.ts',
+});

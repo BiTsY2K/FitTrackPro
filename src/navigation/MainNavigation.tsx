381a20199b88df4eeaac13afca6c6e24f2a57c3c
ROHIT RAI <rohitrai2948@gmail.com>
2026-04-15 23:59:01 +0530

    refactor(food): refactor main navigator

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,13 +1,18 @@
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { ComponentProps } from 'react';
 
-import { Colors } from '@/constants/theme';
 import { DashboardScreen } from '@/screens/dashboard/DashboardScreen';
 import { FoodDetailScreen } from '@/screens/food/FoodDetailScreen';
 import { FoodSearchScreen } from '@/screens/food/FoodSearchScreen';
 import { ProfileScreen } from '@/screens/profile/ProfileScreen';
+import { colors } from '@/themes';
+
+export type BottomTabParamList = {
+  Dashboard: undefined;
+  Profile: undefined;
+};
 
 // ── Navigation param types ── //
 export type MainStackParamList = {
@@ -16,35 +21,29 @@ export type MainStackParamList = {
   FoodDetail: { food: object; mealType: string };
 };
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator<BottomTabParamList>();
 const Stack = createNativeStackNavigator<MainStackParamList>();
 
 /** Bottom tab navigator */
 const TabNavigator = () => (
   <Tab.Navigator
-    screenOptions={{
-      tabBarActiveTintColor: Colors.primary[400],
-      tabBarInactiveTintColor: Colors.gray[400],
-      tabBarStyle: {
-        borderTopColor: Colors.border,
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Dashboard') iconName = focused ? 'home' : 'home-outline';
+        else if (route.name === 'Profile') iconName = focused ? 'person' : 'person-outline';
+
+        return <Ionicons name={iconName as ComponentProps<typeof Ionicons>['name']} size={size} color={color} />;
       },
+
+      tabBarActiveTintColor: colors.brand.dim,
+      tabBarInactiveTintColor: colors.accent.gray,
+      tabBarStyle: { borderTopColor: colors.accent.grayVivid },
       headerShown: false,
-    }}
+    })}
   >
-    <Tab.Screen
-      name="Dashboard"
-      component={DashboardScreen}
-      options={{
-        tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
-      }}
-    />
-    <Tab.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} />,
-      }}
-    />
+    <Tab.Screen name="Dashboard" component={DashboardScreen} options={{}} />
+    <Tab.Screen name="Profile" component={ProfileScreen} options={{}} />
   </Tab.Navigator>
 );
 
